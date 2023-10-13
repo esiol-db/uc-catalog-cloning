cc6fd0572f6abb4200fa65c90d501c4bf8f5b948
esiol-db <esiol-db>
2023-10-13 09:37:47 +0000

    added Schema, Table and view migration

--- eo_dev.py
+++ eo_dev.py
@@ -9,14 +9,18 @@ old_catalog_name = 'eo000_ctg_ext_loc2'
 
 # COMMAND ----------
 
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service import catalog
 def parse_transfer_permissions(securable_type: catalog.SecurableType, old_securable_full_name: str, new_securable_full_name: str) -> int:
   try:
     grants = w.grants.get(securable_type=securable_type, full_name=f'{old_securable_full_name}')
+    if grants.privilege_assignments == None:
+      return 1
     changes = []
     for principal_permission_pair in grants.privilege_assignments:
       principal = principal_permission_pair.principal
-      privilages = [eval('catalog.Privilege.'+privilage) for privilage in principal_permission_pair.privileges if 'VOL' not in privilage]
-      changes.append(catalog.PermissionsChange(add=privilages, principal=principal))
+      privileges = [eval('catalog.Privilege.'+privilege) for privilege in principal_permission_pair.privileges if (('VOL' not in privilege) and ('BROWSE' not in privilege))]
+      changes.append(catalog.PermissionsChange(add=privileges, principal=principal))
     w.grants.update(full_name=new_securable_full_name, securable_type=securable_type, changes=changes)
     return 1
   except Exception as e:
@@ -43,60 +47,32 @@ external_location_name = 'eo000_ext_loc_ctg4'
 external_location_url = 'abfss://eo000ext4@oneenvadls.dfs.core.windows.net/'
 external_location_created = w.external_locations.create(name=external_location_name,
                                                 credential_name=storage_credential_name,
-                                                comment="your comment",
+                                                comment="Created with SDK",
                                                 url=external_location_url)
-
-# COMMAND ----------
-
-external_location
 
 # COMMAND ----------
 
 parse_transfer_permissions(securable_type=catalog.SecurableType.EXTERNAL_LOCATION, 
                            old_securable_full_name=old_external_location_name,
-                           new_securable_full_name=external_location.name)
-
-# COMMAND ----------
-
-grants = w.grants.get(securable_type=catalog.SecurableType.EXTERNAL_LOCATION, full_name=f'{old_external_location_name}')
-changes = []
-for principal_permission_pair in grants.privilege_assignments:
-  principal = principal_permission_pair.principal
-  #privilages = [eval('catalog.Privilege.'+privilage) for privilage in principal_permission_pair.privileges if 'VOL' not in privilage]
-  changes.append(catalog.PermissionsChange(add=principal_permission_pair.privileges, principal=principal))
-# w.grants.update(full_name=external_location.name, securable_type=catalog.SecurableType.EXTERNAL_LOCATION, changes=changes)
-
-
-# COMMAND ----------
-
-changes
+                           new_securable_full_name=external_location_created.name)
 
 # COMMAND ----------
 
 #Create the new catalog
 new_catalog_name = 'eo000_ctg_ext_loc4'
-new_catalog = w.catalogs.create(name=new_catalog_name,
-                            comment='your comment',
-                            storage_root=external_location.url)
+catalog_created = w.catalogs.create(name=new_catalog_name,
+                            comment='Created by SDK',
+                            storage_root=external_location_created.url)
 
 # COMMAND ----------
 
-new_catalog
+catalog_created.name
 
 # COMMAND ----------
 
-grants = w.grants.get(securable_type=catalog.SecurableType.CATALOG, full_name='eo000_ctg_ext_loc2')
-grants
-
-# COMMAND ----------
-
-for principal_permission in grants.privilege_assignments:
-  principal = principal_permission.principal
-  privilages = [eval('catalog.Privilege.'+privilage) for privilage in principal_permission.privileges if privilage not in ['CREATE_VOLUME','READ_VOLUME']]
-
-# COMMAND ----------
-
-privilages
+parse_transfer_permissions(securable_type=catalog.SecurableType.CATALOG, 
+                           old_securable_full_name=old_catalog_name,
+                           new_securable_full_name=catalog_created.full_name)
 
 # COMMAND ----------
 
@@ -105,48 +81,90 @@ privilages
 
 # COMMAND ----------
 
+db.full_name
 
-db = w.schemas.list(old_catalog_name)[0]
+# COMMAND ----------
+
+# old_securable_full_name=db.full_name
+# new_securable_full_name=db_created.full_name
+# securable_type=catalog.SecurableType.SCHEMA
+
+# grants = w.grants.get(securable_type=securable_type, full_name=f'{old_securable_full_name}')
+# changes = []
+# for principal_permission_pair in grants.privilege_assignments:
+#   principal = principal_permission_pair.principal
+#   privileges = [eval('catalog.Privilege.'+privilege) for privilege in principal_permission_pair.privileges if (('VOL' not in privilege) and ('BROWSE' not in privilege))]
+#   changes.append(catalog.PermissionsChange(add=privileges, principal=principal))
+# w.grants.update(full_name=new_securable_full_name, securable_type=securable_type, changes=changes)
+
+# COMMAND ----------
+
+
+db = w.schemas.list(old_catalog_name)[1]
 # db.name
 # db.storage_root
 
 db_created = w.schemas.create(name=db.name,
-                 catalog_name=new_catalog.name,
+                 catalog_name=catalog_created.full_name,
                  storage_root=db.storage_root)
 
-grants = w.grants.get(securable_type=catalog.SecurableType.SCHEMA, full_name=f'{old_catalog_name}.{db.name}')
-changes = []
-for principal_permission_pair in grants.privilege_assignments:
-  principal = principal_permission_pair.principal
-  privilages = [eval('catalog.Privilege.'+privilage) for privilage in principal_permission_pair.privileges if 'VOL' not in privilage]
-  changes.append(catalog.PermissionsChange(add=privilages, principal=principal))
-w.grants.update(full_name=db_created.full_name, securable_type=catalog.SecurableType.SCHEMA, changes=changes)
-
+parse_transfer_permissions(securable_type=catalog.SecurableType.SCHEMA, 
+                           old_securable_full_name=db.full_name,
+                           new_securable_full_name=db_created.full_name)
 
 
 # COMMAND ----------
 
-grants
+# MAGIC %md
+# MAGIC # TABLE LEVEL
 
 # COMMAND ----------
 
-changes
+# MAGIC %md
+# MAGIC ## TABLES
 
 # COMMAND ----------
 
-changes.privilege_assignments[0].principal
+
+tbl = list(w.tables.list(catalog_name=old_catalog_name, schema_name=db.name))[0]
+
+if tbl.table_type == catalog.TableType.MANAGED:
+  spark.sql(f'CREATE TABLE {db_created.full_name}.{tbl.name} DEEP CLONE {tbl.full_name}')
+
+  tbl_created = w.tables.get(full_name=f'{db_created.full_name}.{tbl.name}')
+
+  parse_transfer_permissions(securable_type=catalog.SecurableType.TABLE, 
+                           old_securable_full_name=tbl.full_name,
+                           new_securable_full_name=tbl_created.full_name)
+
 
 # COMMAND ----------
 
-catalog.PermissionsChange.from_dict(changes[0])
+# MAGIC %md
+# MAGIC ## VIEWS
 
 # COMMAND ----------
 
-w.grants.update(securable_type=catalog.SecurableType.CATALOG, full_name='eo000_ctg_ext_loc4', changes=[changes.privilege_assignments[0]])
+import re
 
 # COMMAND ----------
 
-print(list(w.tables.list(catalog_name='eo000_ctg_ext_loc2', schema_name='db1'))[0])
+tbl = list(w.tables.list(catalog_name=old_catalog_name, schema_name=db.name))[0]
+
+
+if tbl.table_type == catalog.TableType.VIEW:
+  view_defenition = tbl.view_definition
+  view_defenition = re.sub(rf'{old_catalog_name}\.', f'{new_catalog_name}.', view_defenition)
+  view_defenition = re.sub(rf'{old_catalog_name}`\.', f'{new_catalog_name}`.', view_defenition)
+
+  spark.sql(view_defenition)
+
+  tbl_created = w.tables.get(full_name=f'{db_created.full_name}.{tbl.name}')
+
+  parse_transfer_permissions(securable_type=catalog.SecurableType.TABLE, 
+                           old_securable_full_name=tbl.full_name,
+                           new_securable_full_name=tbl_created.full_name)
+
 
 # COMMAND ----------
 
